82ef3991dd000fca3d9d64df3a1bf6791d282ce8
Asive Nzwana <aive.nzwana@marstech.co.za>
2025-08-15 11:11:38 +0200

    publish

--- FlockStudy/Program.cs
+++ FlockStudy/Program.cs
@@ -40,6 +40,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//Deployment
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
